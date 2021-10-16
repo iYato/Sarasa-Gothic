5ddeb34e0ca06be9de4078e747a2c4fe3d562914
Yato <me@oha.moe>
2021-10-16 21:07:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 set -x
 
-cd ~
+cd
 apt update
-apt install npm python3 python3-pip ttfautohint git gcc
+apt install npm python3 python3-pip ttfautohint gcc
+pip3 install afdko &
 
-pip3 install afdko
-git clone https://github.com/iYato/Sarasa-Gothic.git
+apt install git 
+git clone https://github.com/iYato/Sarasa-Gothic.git &
 
 cd /tmp
 wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-linux.tar.gz
 tar xzf premake-5.0.0-alpha16-linux.tar.gz
 chmod +x premake5 /usr/local/bin
-cd -
+mv premake5 /usr/local/bin/
+cd
 
 git clone https://github.com/caryll/otfcc.git
 cd otfcc
 premake5 gmake
 cd build/gmake
 make config=release_x64
-cd ../..
-cd bin/release-x64/
+cd ../../bin/release-x64/
 chmod +x otfcc*
 mv otfcc* /usr/local/bin
 
