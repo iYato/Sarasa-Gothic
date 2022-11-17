680e533cd46a78367b99bc3f6c5593ae663cbb0f
iyato <me@oha.moe>
2022-11-17 20:46:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
-cd
+DIR=$(pwd)
+
 apt update
 apt install npm python3 python3-pip ttfautohint gcc -y
 pip3 install afdko
-
-apt install git -y
-git clone https://github.com/iYato/Sarasa-Gothic.git
 
 cd /tmp
 wget https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-linux.tar.gz
 tar xzf premake-5.0.0-alpha16-linux.tar.gz
 chmod +x premake5 /usr/local/bin
 mv premake5 /usr/local/bin/
-cd
 
 git clone https://github.com/caryll/otfcc.git
 cd otfcc
@@ -25,6 +22,6 @@ cd ../../bin/release-x64/
 chmod +x otfcc*
 mv otfcc* /usr/local/bin
 
-cd ~/Sarasa-Gothic
+cd $DIR
 npm install
 npm run build ttc
